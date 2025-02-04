a0dd5681ebfae8704952234e5bfe272473d1abf9
Juan Gabriel Martinez <juanmartinez@MacBook-Pro-de-Juan.local>
2025-02-03 19:24:55 -0500

    add testing library

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,13 +1,42 @@
-/**
- * @format
- */
-
 import React from 'react';
-import ReactTestRenderer from 'react-test-renderer';
-import App from '../App';
+import {render} from '@testing-library/react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {DetailScreen, HomeScreen} from '../src/screens';
+import {RootStackParamList} from '../src/types';
 
-test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+describe('App Navigation', () => {
+  test('renders home screen correctly', () => {
+    const {getByText} = render(
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{title: 'Crypto List'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>,
+    );
+
+    expect(getByText('Crypto List')).toBeTruthy();
+  });
+
+  test('renders detail screen correctly', () => {
+    const {getByText} = render(
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Detail"
+            component={DetailScreen}
+            options={{title: 'Crypto Detail'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>,
+    );
+
+    expect(getByText('Crypto Detail')).toBeTruthy();
   });
 });
